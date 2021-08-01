64ee61560e5aca0171f06898772f64c4804f1da1
Denys Bilonozhko <deformix@gmail.com>
2021-08-01 11:03:11 +0300

    feat: add flex layout

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,18 +3,120 @@ import './Card.css';
 
 const Card = () => {
   return (
-    <li className="card">
-      <img
-        className="card__image"
-        src="https://via.placeholder.com/150"
-        alt=""
-      />
-      <h3 className="card__name">Card Name</h3>
-      <p className="card__author">Author Name</p>
-      <button className="card__button" type="button">
-        View
-      </button>
-    </li>
+    <>
+      <li className="card">
+        <div className="card__content">
+          <img
+            className="card__image"
+            src="https://via.placeholder.com/150"
+            alt=""
+          />
+          <h3 className="card__name">Card Name</h3>
+          <p className="card__author">Author Name</p>
+          <button className="card__button" type="button">
+            View
+          </button>
+        </div>
+      </li>
+      <li className="card">
+        <div className="card__content">
+          <img
+            className="card__image"
+            src="https://via.placeholder.com/150"
+            alt=""
+          />
+          <h3 className="card__name">Card Name</h3>
+          <p className="card__author">Author Name</p>
+          <button className="card__button" type="button">
+            View
+          </button>
+        </div>
+      </li>
+      <li className="card">
+        <div className="card__content">
+          <img
+            className="card__image"
+            src="https://via.placeholder.com/150"
+            alt=""
+          />
+          <h3 className="card__name">Card Name</h3>
+          <p className="card__author">Author Name</p>
+          <button className="card__button" type="button">
+            View
+          </button>
+        </div>
+      </li>
+      <li className="card">
+        <div className="card__content">
+          <img
+            className="card__image"
+            src="https://via.placeholder.com/150"
+            alt=""
+          />
+          <h3 className="card__name">Card Name</h3>
+          <p className="card__author">Author Name</p>
+          <button className="card__button" type="button">
+            View
+          </button>
+        </div>
+      </li>
+      <li className="card">
+        <div className="card__content">
+          <img
+            className="card__image"
+            src="https://via.placeholder.com/150"
+            alt=""
+          />
+          <h3 className="card__name">Card Name</h3>
+          <p className="card__author">Author Name</p>
+          <button className="card__button" type="button">
+            View
+          </button>
+        </div>
+      </li>
+      <li className="card">
+        <div className="card__content">
+          <img
+            className="card__image"
+            src="https://via.placeholder.com/150"
+            alt=""
+          />
+          <h3 className="card__name">Card Name</h3>
+          <p className="card__author">Author Name</p>
+          <button className="card__button" type="button">
+            View
+          </button>
+        </div>
+      </li>
+      <li className="card">
+        <div className="card__content">
+          <img
+            className="card__image"
+            src="https://via.placeholder.com/150"
+            alt=""
+          />
+          <h3 className="card__name">Card Name</h3>
+          <p className="card__author">Author Name</p>
+          <button className="card__button" type="button">
+            View
+          </button>
+        </div>
+      </li>
+      <li className="card">
+        <div className="card__content">
+          <img
+            className="card__image"
+            src="https://via.placeholder.com/150"
+            alt=""
+          />
+          <h3 className="card__name">Card Name</h3>
+          <p className="card__author">Author Name</p>
+          <button className="card__button" type="button">
+            View
+          </button>
+        </div>
+      </li>
+    </>
   );
 };
 
--- src/Components/CardsList/CardsList.jsx
+++ src/Components/CardsList/CardsList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../Card/Card';
+import './CardsList.css';
 
 const CardsList = () => {
   return (
