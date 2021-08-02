6b6fbf6518e3c6fb2b1ce72a0b6ba40057b82bb3
Denys Bilonozhko <deformix@gmail.com>
2021-08-02 20:50:59 +0300

    feat: add validation

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,24 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Form = ({ setFormValues }) => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [birthDate, setBirthDate] = useState('');
-  const [country, setCountry] = useState('Ukraine');
+  const [gender, setGender] = useState('Male');
   const [agree, setAgree] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    setErrors({});
+    if (!agree) {
+      setErrors((state) => ({ ...state, agree }));
+    }
+    if (firstName === '') {
+      setErrors((state) => ({ ...state, firstName }));
+    }
+    if (lastName === '') {
+      setErrors((state) => ({ ...state, lastName }));
+    }
+    if (birthDate === '') {
+      setErrors((state) => ({ ...state, birthDate }));
+    }
+  };
+
+  const resetForm = () => {
+    setAgree(false);
+    setGender('Male');
+    setBirthDate('');
+    setLastName('');
+    setFirstName('');
+  };
+
+  useEffect(() => {
+    validate();
+  }, [agree, firstName, birthDate]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setFormValues((state) => [
-      ...state,
-      { firstName, lastName, birthDate, country, agree },
-    ]);
+    if (Object.keys(errors).length === 0) {
+      setFormValues((state) => [
+        ...state,
+        { firstName, lastName, birthDate, gender, agree },
+      ]);
+    }
+    resetForm();
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="firstName">
-        Name:
+        <p>Name: {errors?.firstName === '' && <span>No name</span>}</p>
         <input
           type="text"
           name="firstName"
@@ -44,22 +76,22 @@ const Form = ({ setFormValues }) => {
           onChange={(event) => setBirthDate(event.target.value)}
         />
       </label>
-      <label htmlFor="country">
-        Country:
+      <label htmlFor="gender">
+        Gender:
         <select
-          name="country"
-          value={country}
+          name="gender"
+          value={gender}
           onChange={(event) => {
-            setCountry(event.target.value);
+            setGender(event.target.value);
           }}
         >
-          <option>Russia</option>
-          <option>Ukraine</option>
-          <option>Europe</option>
+          <option>Male</option>
+          <option>Female</option>
+          <option>Other</option>
         </select>
       </label>
       <label htmlFor="agree">
-        Agree
+        <p>Agree {errors?.agree !== undefined && <span>Not checked</span>}</p>
         <input
           type="checkbox"
           name="agree"
