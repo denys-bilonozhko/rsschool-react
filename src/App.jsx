4881adcb13518d382646449fe6ccf12666cadf26
Denys Bilonozhko <deformix@gmail.com>
2021-08-03 21:18:26 +0300

    feat: implemented save to the localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
-
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import Form from './Components/Form/Form';
+import Card from './Components/Card/Card';
+import './App.css';
 
 const App = () => {
-  const [formValues, setFormValues] = useState([]);
+  const [formValues, setFormValues] = useState(
+    JSON.parse(localStorage.getItem('myData')) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('myData', JSON.stringify(formValues));
+    console.log(localStorage.getItem('myData'));
+  }, [formValues]);
   return (
-    <div>
-      <Form setFormValues={setFormValues} />
-    </div>
+    <>
+      <div>
+        <Form setFormValues={setFormValues} formValues={formValues} />
+      </div>
+      <ul>
+        {formValues.map((item) => {
+          return <Card item={item} key={Math.floor(Math.random() * 100)} />;
+        })}
+      </ul>
+    </>
   );
 };
 
